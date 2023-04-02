89a3857e96450bbb15fbb024127ba244b17a4589
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-04-01 23:20:01 -0400

    next commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.interpolate import Rbf as RBF
 from matplotlib import pyplot as plt
 from math import dist
+from random import randrange
 
 def new_point(a, b, t):
     return [a[i]*(1-t)+b[i]*t for i in range(0,3)]
@@ -16,17 +17,12 @@ def new_edge(a,b,ss):
         new_points.append(new_point(a,b,step*ss/dist(a,b)))
     return new_points
 
-# points = [[0,0,2], [3,0,4], [3,4,2], [0,4,4]]
-input_points = np.array([[0,0,1], [3,0,3], [3,4,2], [0,4,3]])
-input_points = np.array([[0,0,10], [3,0,30], [3,4,20], [0,4,30]])
+input_points = np.array([[0,0,100], [3,0,300], [3,4,200], [0,4,300]])
 ss = step_size(input_points)
 
-# z_min = input_points[:,2].min()
-# lowered_point = np.add(input_points,[0,0,-1*z_min])
-
 scale = 100
-points = np.multiply(input_points,[scale, scale, 1])
-ss = ss*scale
+points = np.multiply(input_points,[scale, scale, 1])  # scale up the x, y values
+ss = ss * scale
 
 nps = []
 clpts = np.append(points, [points[0]], axis=0)
@@ -56,7 +52,12 @@ yi = np.linspace(y_min, y_max, 200)
 XI, YI = np.meshgrid(xi, yi)
 ZI = rbf(XI,YI)
 
+rand_x = randrange(round(x_max-x_min,0))+x_min
+rand_y = randrange(round(y_max-y_min,0))+y_min
+rand_z = rbf(rand_x, rand_y)
+
 ax.scatter(x, y, z, c='red')
 ax.plot_wireframe(XI, YI, ZI, rstride=10, cstride=10)
+ax.scatter(rand_x, rand_y, rand_z, c='blue')
 
 plt.show()
