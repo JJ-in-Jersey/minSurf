7d170f95d1af42e73a41b7c0b173374c8dc09dda
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-04-12 01:11:17 -0400

    next commit

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import numpy as np
 from numpy.linalg import norm as length
-from sympy.geometry import Point
-from sympy.geometry.line import Segment
+from sympy.geometry import Point, Line, Segment, Plane
+from sympy import symbols
 
 from scipy.interpolate import Rbf as RBF
 from matplotlib import pyplot as plot
 from random import randrange
 from time import sleep
-
-def z_intercept(vector):
-    x0 = vector[0][0]
-    y0 = vector[0][1]
-    z0 = vector[0][2]
-    delta_x = vector[1][0]-x0
-    delta_y = vector[1][1]-y0
-    delta_z = vector[1][2]-z0
-    y = -1*z0*delta_y/delta_z+y0
-    x = -1*z0*delta_x/delta_z+x0
-    return np.array([x, y, 0])
 
 def translate_3D(points, vector):
     delta_x = vector[1][0] - vector[0][0]
@@ -37,37 +26,43 @@ class VelocitySurface:
     scale = 100
     LINE = 'LINE'
     SURFACE = 'SURFACE'
-
-    @staticmethod
-    def new_point(p1: Point, p2: Point, t): return (p1.scale(1-t, 1-t, 1-t)+p2.scale(t, t, t)).evalf()
+    XY_PLANE = Plane(Point(0, 0, 0), normal_vector=[0, 0, 1])
 
     @staticmethod
     def step_size(segments): return np.array([s.length for s in segments]).min()/10
 
-    def edge_points(self, p1: Point, p2: Point, ss):
-        num_pts = range(1, int(round(p1.distance(p2)/ss, 0)))
-        return [self.new_point(p1, p2, i*ss/p1.distance(p2)) for i in num_pts]
+    @staticmethod
+    def edge_points(segment: Segment, ss):
+        t = symbols('t')
+        num_pts = range(0, int(round(segment.length/ss, 0)))
+        return [segment.arbitrary_point(t).evalf(subs={t:i*ss/segment.length}) for i in num_pts]
 
-    def plot_vector(self, vector, color, style):
-        self.ax.plot3D(x_values(vector), y_values(vector), z_values(vector), c=color, linestyle=style)
+    def plot_segment(self, segment, color, style):
+        points = np.array(segment.points).astype(float)
+        self.ax.plot3D(x_values(points), y_values(points), z_values(points), c=color, linestyle=style)
 
     def plot_point(self, point, color, mark):
         self.ax.scatter(point[0], point[1], point[2], c=color, marker=mark)
 
     def show_axes(self):
+        self.ax = plot.axes(projection="3d")
         xi = np.linspace(self.x_min, self.x_max, 200)
         yi = np.linspace(self.y_min, self.y_max, 200)
         XI, YI = np.meshgrid(xi, yi)
         if self.shape == VelocitySurface.SURFACE:
             ZI = self.surface(XI, YI)
 
+        self.plot_point(Point(0,0,0), 'black', '.')
         self.ax.scatter(self.edge_x, self.edge_y, self.edge_z, c='orange', marker='.')
         self.ax.scatter(self.scaled_x, self.scaled_y, self.scaled_z, c='black', marker='.')
+
         if self.shape == VelocitySurface.LINE:
-            self.plot_vector(self.base_vector, 'lightgrey', 'solid')
-            self.plot_vector(VelocitySurface.vector(z_intercept(self.base_vector), self.base_vector[0]), 'grey', 'dotted')
+            self.plot_segment(self.base_vector, 'lightgrey', 'solid')
+            z_intercept = VelocitySurface.XY_PLANE.intersection(Line(self.base_vector))[0]
+            self.plot_segment(Segment(z_intercept, self.base_vector.p1), 'grey', 'dotted')
         if self.shape == VelocitySurface.SURFACE:
             self.ax.plot_wireframe(XI, YI, ZI, rstride=10, cstride=10, color='grey', linewidth=0.25)
+
         plot.show(block=False)
         plot.pause(0.01)
 
@@ -94,32 +89,23 @@ class VelocitySurface:
             if not isinstance(pt, Point): raise TypeError
         self.points = points
 
-        if len(points) == 2:
-            self.shape = VelocitySurface.LINE
-        else:
-            self.shape = VelocitySurface.SURFACE
+        if len(points) == 2: self.shape = VelocitySurface.LINE
+        else: self.shape = VelocitySurface.SURFACE
 
         scaled_points = [pt.scale(VelocitySurface.scale, VelocitySurface.scale, 1) for pt in points]
         segments = [Segment(pt, scaled_points[i+1]) for i, pt in enumerate(scaled_points[:-1])]
 
-        if self.shape == VelocitySurface.SURFACE:  # close the figure
-            segments.append(Segment(scaled_points[-1], scaled_points[0]))
-        elif self.shape == VelocitySurface.LINE:
-            self.base_vector = Segment[0]
+        if self.shape == VelocitySurface.SURFACE: segments.append(Segment(scaled_points[-1], scaled_points[0]))  # close the figure
+        elif self.shape == VelocitySurface.LINE: self.base_vector = segments[0]
 
         ss = self.step_size(segments)
-        self.ax = plot.axes(projection="3d")
-
         points = [scaled_points[0]] + [s.points[1] for s in segments]
-
         edge_points = []
-        for i, pt in enumerate(points[:-1]):
-            edge_points += [pt] + self.edge_points(pt, points[i+1], ss)
+        for segment in segments:
+            edge_points += self.edge_points(segment, ss)
 
-        if self.shape == 'SURFACE':
-            edge_points.append(scaled_points[0])
-        elif self.shape == 'LINE':
-            edge_points.append(scaled_points[-1])
+        if self.shape == 'SURFACE': edge_points.append(scaled_points[0])
+        elif self.shape == 'LINE': edge_points.append(scaled_points[-1])
 
         points = np.array(points).astype(float)
         edge_points = np.array(edge_points).astype(float)
@@ -145,8 +131,8 @@ p2 = Point(3, 0, 300)
 p3 = Point(3, 4, 200)
 p4 = Point(0, 4, 300)
 
-vs = VelocitySurface(p1,p2,p3,p4)
-# vs = VelocitySurface(p1,p3)
+# vs = VelocitySurface(p1,p2,p3,p4)
+vs = VelocitySurface(p1,p3)
 vs.show_axes()
 
 for wait in range(0,50):
@@ -157,7 +143,8 @@ for wait in range(0,50):
         vs.plot_point(Point(rand_x, rand_y, 0), 'grey', '.')
         vs.plot_point(vs.get_velocity(Point(rand_x, rand_y, 0)), 'red', '.')
     elif vs.shape == vs.LINE:
-        vs.plot_vector(vs.vector(z_intercept(vs.base_vector), np.array([rand_x, rand_y, 0])), 'lightgrey', 'dotted')
+        z_intercept = vs.XY_PLANE.intersection(Line(vs.base_vector))[0]
+        vs.plot_segment(Segment(z_intercept, Point(rand_x, rand_y, 0)), 'lightgrey', 'dotted')
         vs.plot_point(Point(rand_x, rand_y, 0), 'lightgrey', '.')
         vs.plot_point(vs.get_velocity(Point(rand_x, rand_y, 0)), 'red', '.')
 
